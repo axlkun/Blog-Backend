3d514f85f52a39950ae11faf937063204224b3e0
axlkun <axelcruz.dev@gmail.com>
2023-09-30 19:32:00 -0600

    changing default view content

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
+// import Welcome from '@/Components/Welcome.vue';
 </script>
 
 <template>
@@ -14,7 +14,7 @@ import Welcome from '@/Components/Welcome.vue';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+                    Axel Dashboard
                 </div>
             </div>
         </div>
