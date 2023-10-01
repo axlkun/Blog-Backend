164b262c5520d3ba0a42bcf2d57d89188106e4c5
axlkun <axelcruz.dev@gmail.com>
2023-10-01 15:36:22 -0600

    change the dinamic title

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,8 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+// const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = 'AxelPortfolio';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
