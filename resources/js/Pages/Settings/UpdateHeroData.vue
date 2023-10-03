ae9a90011ae6c7b79fd3b5b5127321e6d8bfe0ca
axlkun <axelcruz.dev@gmail.com>
2023-10-02 20:41:44 -0600

    add v-model and emit to make reactive the image

--- resources/js/Pages/Settings/UpdateHeroData.vue
+++ resources/js/Pages/Settings/UpdateHeroData.vue
@@ -6,68 +6,26 @@ import FormSection from '@/Components/FormSection.vue';
 import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
-import SecondaryButton from '@/Components/SecondaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
 import AppTexArea from '@/OwnComponents/TextArea.vue';
+import AppImage from '@/OwnComponents/Image.vue';
 
 const props = defineProps({
     settings: Object,
 });
+
 
 const form = useForm({
     description: props.settings.data.description,
     photo: null,
 });
 
-const verificationLinkSent = ref(null);
-const photoPreview = ref(null);
-const photoInput = ref(null);
-
 const updateHeroInformation = () => {
-    if (photoInput.value) {
-        form.photo = photoInput.value.files[0];
-    }
-
+    
     form.post(route('settings.save-hero'), {
-        errorBag: 'updateProfileInformation',
         preserveScroll: true,
-        onSuccess: () => clearPhotoFileInput(),
     });
 };
 
-const selectNewPhoto = () => {
-    photoInput.value.click();
-};
-
-const updatePhotoPreview = () => {
-    const photo = photoInput.value.files[0];
-
-    if (! photo) return;
-
-    const reader = new FileReader();
-
-    reader.onload = (e) => {
-        photoPreview.value = e.target.result;
-    };
-
-    reader.readAsDataURL(photo);
-};
-
-// const deletePhoto = () => {
-//     router.delete(route('current-user-photo.destroy'), {
-//         preserveScroll: true,
-//         onSuccess: () => {
-//             photoPreview.value = null;
-//             clearPhotoFileInput();
-//         },
-//     });
-// };
-
-const clearPhotoFileInput = () => {
-    if (photoInput.value?.value) {
-        photoInput.value.value = null;
-    }
-};
 </script>
 
 <template>
@@ -83,44 +41,10 @@ const clearPhotoFileInput = () => {
         <template #form>
             <!-- Profile Photo -->
             <div v-if="$page.props.jetstream.managesProfilePhotos" class="col-span-6 sm:col-span-4">
-                <!-- Profile Photo File Input -->
-                <input
-                    ref="photoInput"
-                    type="file"
-                    class="hidden"
-                    @change="updatePhotoPreview"
-                >
+                
+                <AppImage :imageUrl="settings.data.photo_url" :imageAlt="settings.data.alt_text" label="Photo" :error-message="form.errors.photo" v-model="form.photo"></AppImage>
 
-                <InputLabel for="photo" value="Photo" />
-
-                <!-- Current Profile Photo -->
-                <div v-show="! photoPreview" class="mt-2">
-                    <img :src="settings.data.photo_url" :alt="settings.data.alt_text" class="rounded-full h-20 w-20 object-cover">
-                </div>
-
-                <!-- New Profile Photo Preview -->
-                <div v-show="photoPreview" class="mt-2">
-                    <span
-                        class="block rounded-full w-20 h-20 bg-cover bg-no-repeat bg-center"
-                        :style="'background-image: url(\'' + photoPreview + '\');'"
-                    />
-                </div>
-
-                <SecondaryButton class="mt-2 mr-2" type="button" @click.prevent="selectNewPhoto">
-                    Select A New Photo
-                </SecondaryButton>
-
-                <!-- <SecondaryButton
-                    v-if="user.profile_photo_path"
-                    type="button"
-                    class="mt-2"
-                    @click.prevent="deletePhoto"
-                >
-                    Remove Photo
-                </SecondaryButton> -->
-
-                <InputError :message="form.errors.photo" class="mt-2" />
-            </div>
+            </div> 
 
             <!-- Description -->
             <div class="col-span-6 sm:col-span-6">
