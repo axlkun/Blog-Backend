b1b36e35934486d7a68de4e685bd922a88a5975d
axlkun <axelcruz.dev@gmail.com>
2023-10-07 14:08:14 -0600

    add padding

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -13,7 +13,7 @@ import AppLayout from '@/Layouts/AppLayout.vue';
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="p-6 bg-white overflow-hidden shadow-xl sm:rounded-lg">
                     Main content
                 </div>
             </div>
