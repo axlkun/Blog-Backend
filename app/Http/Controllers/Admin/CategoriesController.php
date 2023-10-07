6ffdce770cdeaef769b3033d203db02f83da92e0
axlkun <axelcruz.dev@gmail.com>
2023-10-07 13:31:17 -0600

    add view to categories

--- app/Http/Controllers/Admin/CategoriesController.php
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CategoriesController extends Controller
 {
-    //
+    public function index(Request $request){
+        return Inertia::render('Categories/Index',[
+            'categories' => CategoryResource::collection(Category::latest()->simplePaginate(10))
+        ]);
+    }
 }
