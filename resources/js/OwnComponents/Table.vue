b0494f340242c91a7029c08eac6f0658e37dcc59
axlkun <axelcruz.dev@gmail.com>
2023-10-10 13:23:34 -0600

    convert table into component

---
+++ resources/js/OwnComponents/Table.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+    headers: {
+        type: Array,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th v-for="(header,index) in headers" :key="`header-${index}`" :class="header.class || 'text-left'">{{header.name}}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <slot></slot>
+        </tbody>
+    </table>
+</template>
--- resources/js/Pages/Categories/Index.vue
+++ resources/js/Pages/Categories/Index.vue
@@ -2,12 +2,22 @@
 import AppLayout from '@/Layouts/AppLayout.vue';
 import EditBtn from '@/OwnComponents/EditBtn.vue';
 import DeleteBtn from '@/OwnComponents/DeleteBtn.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
 import SimplePagination from '@/OwnComponents/SimplePagination.vue';
+import AppTable from '@/OwnComponents/Table.vue';
 
 const props = defineProps({
     categories: Object,
 });
+
+const headers = [
+    { name: "Name" },
+    { name: "Slug" },
+    {
+        name: "Actions",
+        class: "text-right"
+    }
+];
+
 </script>
 
 <template>
@@ -21,7 +31,22 @@ const props = defineProps({
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="p-6 bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <table>
+                    <AppTable :headers="headers">
+                        <tr v-for="category in categories.data" :key="category.id">
+                            <td>{{ category.name }}</td>
+                            <td>{{ category.slug }}</td>
+                            <td>
+                                <div class="flex items-center justify-end space-x-2">
+                                    <EditBtn :url="route('categories.edit', { category: category.id })"></EditBtn>
+
+                                    <DeleteBtn :url="route('categories.destroy', { category: category.id })"
+                                        module-name="category"></DeleteBtn>
+                                </div>
+                            </td>
+                        </tr>
+                    </AppTable>
+
+                    <!-- <table>
                         <thead>
                             <tr>
                                 <th class="text-left">Name</th>
@@ -42,17 +67,14 @@ const props = defineProps({
                                 </td>
                             </tr>
                         </tbody>
-                    </table>
+                    </table> -->
 
                     <div class="mt-10">
-                        <!-- <div class="flex items-center justify-between">
-                            <PrimaryButton :href="categories.links.prev" :disable="!categories.links.prev" :class="{'opacity-25': !categories.links.prev}">Previous</PrimaryButton>
-                            <PrimaryButton :href="categories.links.next" :disable="!categories.links.next" :class="{'opacity-25': !categories.links.next}">Next</PrimaryButton>
-                        </div> -->
-                        <SimplePagination :prevUrl="categories.links.prev" :nextUrl="categories.links.next"></SimplePagination>
+
+                        <SimplePagination :prevUrl="categories.links.prev" :nextUrl="categories.links.next">
+                        </SimplePagination>
                     </div>
                 </div>
             </div>
         </div>
-    </AppLayout>
-</template>
+</AppLayout></template>
