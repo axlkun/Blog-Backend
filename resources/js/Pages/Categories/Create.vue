fbd3615d9a0c2d3613a06e4d318c935f0cbbbec1
axlkun <axelcruz.dev@gmail.com>
2023-10-11 10:48:52 -0600

    add container and card component

--- resources/js/Pages/Categories/Create.vue
+++ resources/js/Pages/Categories/Create.vue
@@ -9,6 +9,8 @@ import InputLabel from '@/Components/InputLabel.vue';
 import TextInput from '@/Components/TextInput.vue';
 import ActionMessage from '@/Components/ActionMessage.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
+import Container from '@/OwnComponents/Container.vue';
+import Card from '@/OwnComponents/Card.vue';
 
 const form = useForm({
     name: "",
@@ -36,36 +38,35 @@ const saveCategory = () => {
             </h2>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="p-6 bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <form @submit.prevent="saveCategory">
-                        <div>
-                            <InputLabel for="name" value="Name" />
-                            <TextInput id="name" v-model="form.name" type="text" class="mt-1 block w-full" required
-                                autocomplete="name" />
-                            <InputError :message="form.errors.name" class="mt-2" />
-                        </div>
+        <Container>
+            <Card>
+                <form @submit.prevent="saveCategory">
+                    <div>
+                        <InputLabel for="name" value="Name" />
+                        <TextInput id="name" v-model="form.name" type="text" class="mt-1 block w-full" required
+                            autocomplete="name" />
+                        <InputError :message="form.errors.name" class="mt-2" />
+                    </div>
 
-                        <div class="mt-4">
-                            <InputLabel for="slug" value="Slug" />
-                            <TextInput id="slug" v-model="form.slug" type="text" class="mt-1 block w-full" required
-                                autocomplete="slug" />
-                            <InputError :message="form.errors.slug" class="mt-2" />
-                        </div>
+                    <div class="mt-4">
+                        <InputLabel for="slug" value="Slug" />
+                        <TextInput id="slug" v-model="form.slug" type="text" class="mt-1 block w-full" required
+                            autocomplete="slug" />
+                        <InputError :message="form.errors.slug" class="mt-2" />
+                    </div>
 
-                        <div class="mt-4">
-                            <ActionMessage :on="form.recentlySuccessful" class="mr-3">
-                                Saved.
-                            </ActionMessage>
+                    <div class="mt-4">
+                        <ActionMessage :on="form.recentlySuccessful" class="mr-3">
+                            Saved.
+                        </ActionMessage>
 
-                            <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                                Save
-                            </PrimaryButton>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+                        <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                            Save
+                        </PrimaryButton>
+                    </div>
+                </form>
+            </Card>
+        </Container>
+            
     </AppLayout>
 </template>
