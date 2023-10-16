db450c7c83ac37d4961a790b4f94a8f519f78df8
axlkun <axelcruz.dev@gmail.com>
2023-10-16 13:41:51 -0600

    create email method

--- app/Http/Controllers/Api/ContactUsController.php
+++ app/Http/Controllers/Api/ContactUsController.php
@@ -10,9 +10,10 @@ use Illuminate\Support\Facades\Mail;
 class ContactUsController extends Controller
 {
     public function store(Request $request){
+        
         $data = $request->validate([
             'name' => ['required','string','max:100'],
-            'email' => ['required','string','mail','max:100'],
+            'email' => ['required','string','email','max:100'],
             'phone' => ['required','string','max:20'],
             'message' => ['required','string','max:500']
         ]);
