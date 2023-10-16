ff0528160941f87973bd88c928d42fbfe47346dd
axlkun <axelcruz.dev@gmail.com>
2023-10-15 22:06:53 -0600

    create index method to show data

--- app/Http/Controllers/Api/SettingsController.php
+++ app/Http/Controllers/Api/SettingsController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\SettingsResource;
+use App\Models\Settings;
 use Illuminate\Http\Request;
 
 class SettingsController extends Controller
 {
-    //
+    public function index(Request $request){
+        return new SettingsResource(Settings::find(1));
+    }
 }
