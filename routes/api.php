6cbb29906418f32ea97c13213d1cf419675c146d
axlkun <axelcruz.dev@gmail.com>
2023-10-15 22:01:02 -0600

    add settings endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\SettingsController;
 use App\Http\Controllers\Api\ArticlesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('articles',[ArticlesController::class,'index']);
 Route::get('articles/{article:slug}',[ArticlesController::class,'show']);
+Route::get('settings',[SettingsController::class,'index']);
